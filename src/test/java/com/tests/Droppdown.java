10a7953723968bce9eb8aab90d7d3f2ba9443327
husamettin <husamettinszn@gmail.com>
2022-01-28 19:58:05 +0100

    dropdown

--- src/test/java/com/tests/Droppdown.java
+++ src/test/java/com/tests/Droppdown.java
@@ -1,4 +1,30 @@
 package com.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class Droppdown {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        }
+
+     @Test
+     public void dropdownTest(){
+
+
+     }
+
 }
