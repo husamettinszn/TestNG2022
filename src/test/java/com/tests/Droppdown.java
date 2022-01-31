def534ddfe63f3f0d4afbe6b5a8f0ce38d2634f9
husamettin <husamettinszn@gmail.com>
2022-01-31 14:16:37 +0100

    priority

--- src/test/java/com/tests/Droppdown.java
+++ src/test/java/com/tests/Droppdown.java
@@ -8,8 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Droppdown {
@@ -20,13 +22,25 @@ public class Droppdown {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
         }
 
+
      @Test
-     public void dropdownTest() throws InterruptedException {
+
+     public void list(){
+         List<WebElement> drop =driver.findElements(By.xpath("//select[@class='nav-search-dropdown searchSelect nav-progressive-attrubute nav-progressive-search-dropdown']"));
+
+         for (WebElement w: drop
+              ) {
+             System.out.println(w.getText());
+         }
+     }
+    @Test
+
+    public void dropdownTest() throws InterruptedException {
 
         WebElement drop =driver.findElement(By.xpath("//select[@class='nav-search-dropdown searchSelect nav-progressive-attrubute nav-progressive-search-dropdown']"));
 
@@ -36,15 +50,12 @@ public class Droppdown {
         driver.findElement(By.id("nav-search-submit-button")).click();
 
         WebElement produkt11 = driver.findElement(By.xpath("(//span[@class='a-size-base-plus a-color-base a-text-normal'])[11]"));
-         System.out.println(produkt11.getText());
-         produkt11.click();
+        System.out.println(produkt11.getText());
+        produkt11.click();
 
-         driver.findElement(By.xpath("(//span[@class='a-button-inner'])[7]")).click();
-
-
-
-
-     }
+        driver.findElement(By.xpath("(//span[@class='a-button-inner'])[7]")).click();
+        driver.navigate().back();
+    }
      @AfterClass
     public void teardown(){
         driver.close();
