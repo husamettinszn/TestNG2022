e66a0c6a1504da8b6fb09224f8bc284e0a0214a3
husamettin <husamettinszn@gmail.com>
2022-02-01 11:43:58 +0100

    dependon

--- src/test/java/com/tests/Priority.java
+++ src/test/java/com/tests/Priority.java
@@ -25,12 +25,29 @@ public class Priority {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
-    @Test
+    @Test(priority = 1)
     public void suchFeld(){
         driver.get("https://www.youtube.com");
         WebElement suchFeld = driver.findElement(By.xpath("//div[@id='search-input']"));
         Assert.assertTrue(suchFeld.isEnabled());
 
+    }
+    @Test(priority = 2)
+    public void image() throws InterruptedException {
+        driver.get("https://www.youtube.com");
+        WebElement imageYoutube= driver.findElement(By.xpath("(//yt-icon[@id='logo-icon'])[2]"));
+        Assert.assertTrue(imageYoutube.isDisplayed());
+    }
+    @Test(priority = 3)
+    public void falscheTitel(){
+        driver.get("https://www.youtube.com");
+        String falsceTitel= "youtube";
+        Assert.assertFalse(falsceTitel.equals(driver.getTitle()));
+    }@Test(priority = 4)
+    public void titel(){
+        String titel = "YouTube";
+        Assert.assertTrue(titel.equals(driver.getTitle()));
+        System.out.println(driver.getTitle());
     }
     @AfterClass
     public void teardown(){
