c5493db4df0c2f9c33e85ea60dfdd75993fa271a
husamettin <husamettinszn@gmail.com>
2022-02-08 12:35:54 +0100

    Iframe

--- src/test/java/com/tests/IFrame.java
+++ src/test/java/com/tests/IFrame.java
@@ -1,4 +1,48 @@
 package com.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class IFrame {
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void iFrame() throws InterruptedException {
+        driver.get("https://html.com/tags/iframe/");
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        //1.adim iFrame'e nasil gecis yapacagimiza(switch) karar veririz
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='lazy-loaded']"));
+
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+
+        //  index,id,webElement olarak locate ederiz
+        //  biz bu soruda 3.yolu tercih ettik
+
+        Thread.sleep(5000);
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+    }
 }
