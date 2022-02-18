f7c7abcd1820d1945f0f24871dc8f6acfe66aaf2
husamettin <husamettinszn@gmail.com>
2022-02-18 21:38:03 +0100

    W2

--- src/test/java/com/tests/WebTables2.java
+++ src/test/java/com/tests/WebTables2.java
@@ -1,6 +1,7 @@
 package com.tests;
 
 import com.utilities.TestBase;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -13,6 +14,7 @@ public class WebTables2 extends TestBase {
     public void test(){
         driver.get("https://demoqa.com/webtables");
 
+        /*
         List<WebElement> titels = driver.findElements(By.className("rt-resizable-header-content"));
 
         for (WebElement w : titels
@@ -40,5 +42,40 @@ public class WebTables2 extends TestBase {
         List<WebElement> anzahlDerZeile = driver.findElements(By.className("rt-tr-group"));
         System.out.println(anzahlDerZeile.size());
 
+        List<WebElement> anzahlDerSäule = driver.findElements(By.className("rt-resizable-header-content"));
+        System.out.println("Anzahl der Säule :"+anzahlDerSäule.size());
+
+        List<WebElement>  säuleDreiZelle = driver.findElements(By.xpath("//div[@class='rt-tr-group']//div[3]"));
+        for (WebElement w: säuleDreiZelle
+             ) {
+            System.out.println(w.getText());
+        }
+        for (int i=1; i<säuleDreiZelle.size(); i++){
+            WebElement säule3 = driver.findElement(By.xpath("(//div[@class='rt-tr-group']//div[3])["+i+"]"));
+            System.out.println(säule3.getText());
+        }
+
+         */
+        //Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
+        List<WebElement> lastName = driver.findElements(By.xpath("//div[@class='rt-tr-group']//div[2]"));
+
+        List<WebElement> headers = driver.findElements(By.className("rt-resizable-header-content"));
+        int emailZelle =0;
+
+        for (int i=0; i<headers.size(); i++){
+            if (headers.get(i).getText().equals("Email")){
+                emailZelle = i+1;
+            }
+        }
+
+        int zeile=0;
+
+        for (int i=0; i<lastName.size(); i++){
+            if (lastName.get(i).getText().equals("Vega")){
+                zeile = i+1;
+            }
+        }
+        WebElement emailVega= driver.findElement(By.xpath("(//div[@class='rt-tr-group'])["+zeile+"]//div["+emailZelle+"]"));
+        System.out.println(emailVega.getText());
     }
 }
